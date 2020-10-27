1be4479ce54aa1281985011fd79633aef4503299
Prashant Gupta <prashantkumargupta33@gmail.com>
2020-10-27 12:33:29 +0530

    Theory update

--- Salesforce CPQ Plugins/fieldEditable.js
+++ Salesforce CPQ Plugins/fieldEditable.js
@@ -1,6 +1,7 @@
 
 /**
  * The calculator calls this method after it completes a calculation. The method helps to make fields editable or un-editbale on the QLE from the plugin.
+ * This code should be made part of your QCP code if you already have one. Else create a Plugin record in Saleforce and add the name in install package settings
  * @param
  * "fieldName" : api name of the field being evaluated
  * "quoteLineModelRecord" : specific quote lines in a quote being evaluated.
--- Salesforce CPQ Plugins/hideField.js
+++ Salesforce CPQ Plugins/hideField.js
@@ -2,6 +2,7 @@
 
 /**
  * The calculator calls this method after it completes a calculation.  The method helps to make fields visible or hidden on the QLE from the plugin.
+ * This code should be made part of your QCP code if you already have one. Else create a Plugin record in Saleforce and add the name in install package settings
  * @param
  * "fieldName" : api name of the field being evaluated
  * "quoteLineModelRecord" : specific quote lines in a quote being evaluated.
